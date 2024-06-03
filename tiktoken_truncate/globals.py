bfe6417229c299e01eb1122263647a3ec6277823
Joseph Turian <>
2024-06-03 21:41:05 +0200

    Temporarily small max tokens for testing

--- tiktoken_truncate/globals.py
+++ tiktoken_truncate/globals.py
@@ -2,6 +2,8 @@
 
 # A dictionary mapping models to their maximum tokens
 model_max_tokens = {
-    "text-embedding-3-large": 8192,
+    # "text-embedding-3-large": 8192,
+    # Temporary change for testing
+    "text-embedding-3-large": 16,
     # Add other models and their corresponding max tokens here
 }
